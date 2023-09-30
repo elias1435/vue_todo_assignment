2be88d0ac75569886d05727609f3cea90f5213c6
elias1435 <elias1435@gmail.com>
2023-09-30 10:01:09 +0600

    initial-commit

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>        
+    <div class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
+        <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
+            <div class="relative">
+                <div class="absolute">
+                    <div class="">
+                        <h1 class="my-2 text-gray-800 font-bold text-2xl">
+                            Ostad TODO Assignment
+                        </h1>
+                        <p class="mb-10 my-2 text-gray-800">Start with below button</p>
+                        <router-link to="/todo"  class="sm:w-full lg:w-auto my-2 border rounded md py-4 px-8 text-center bg-red-600 text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-700 focus:ring-opacity-50 mt-10">Start</router-link>
+                    </div>
+                </div>
+                <div>
+                    <!-- <img src="https://i.ibb.co/G9DC8S0/200-2.png" /> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/Register.vue
@@ -0,0 +1,49 @@
+<script setup>
+import {ref, reactive} from 'vue';
+import { RouterLink } from 'vue-router';
+import authStore from '../store/authStore';
+const auth = authStore();
+
+const formData = reactive({});
+</script>
+<template>
+    <div class="min-h-screen w-screen bg-gradient-to-l to-red-200 from-slate-200 flex items-center">
+        <div class="max-w-40 w-96 mx-auto  bg-white flex flex-col md:flex-row gap-10 p-5 rounded-lg shadow-lg">
+
+            <div class="w-full">
+                <h1 class="text-2xl font-bold text-gray-600">Get started</h1>
+                <p class="text-sm text-gray-500 pb-10">Create your account now</p>
+
+                <form action="" class="flex flex-col gap-4">
+                    <div>
+                        <label for="name" class="text-gray-500 inline-block pb-2">Full Name</label>
+                        <input v-model="formData.fullName"  class="w-full border-gray-200 px-4 py-2 focus:outline-none" type="text" name="name" placeholder="Full Name">
+                    </div>
+
+                    <div>
+                        <label class="text-gray-500 inline-block pb-2" for="email">Email</label>
+                        <input v-model="formData.email" class="w-full border-gray-200 px-4 py-2 focus:outline-none" type="email" name="email" placeholder="yourmail@example.com">
+                    </div>
+                    <div>
+                        <label class="text-gray-500 inline-block pb-2" for="password">Password</label>
+                        <input v-model="formData.password" class="w-full border-gray-200 px-4 py-2 focus:outline-none" type="password" name="password" placeholder="******">
+                    </div>
+
+                    <div>
+                        <label class="text-gray-500 inline-block pb-2" for="confirm-password">Confirm Password</label>
+                        <input v-model="formData.confirm_password" class="w-full border-gray-200 px-4 py-2 focus:outline-none" type="password" name="confirm-password" placeholder="******">
+                    </div>
+
+                    <div>
+                        <input @click.prvent="auth.register(formData)" class="w-full bg-blue-600 py-2 rounded-md text-white font-bold cursor-pointer" type="button" value="Sign Up">
+                    </div>
+                </form>
+                
+                <p class="text-center pt-5">Already have an account ? <router-link to="/login" class="text-blue-600 font-bold">Sign in</router-link></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
